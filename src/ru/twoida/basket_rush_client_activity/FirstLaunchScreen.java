c22295bdc3bfcfe76048c2dc4df12ff5af414571
Ильнур <1er4yy@gmail.com>
2013-10-05 15:11:42 +0400

    Добавил: Экран первого запуска

---
+++ src/ru/twoida/basket_rush_client_activity/FirstLaunchScreen.java
@@ -0,0 +1,16 @@
+package ru.twoida.basket_rush_client_activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import ru.twoida.basket_rush_client.R;
+
+public class FirstLaunchScreen extends Activity {
+	
+	@Override
+	protected void onCreate (Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.first_launch_screen);
+	}
+		
+	
+}
