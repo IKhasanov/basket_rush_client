ab96ea1198d73590e0ab3f0fcefb870929fec74c
Ильнур <1er4yy@gmail.com>
2013-10-05 16:04:29 +0400

    Добавил: Экран приглашения (Выбор жены/мужа)

---
+++ src/ru/twoida/basket_rush_client_activity/InviteScreen.java
@@ -0,0 +1,13 @@
+package ru.twoida.basket_rush_client_activity;
+
+import ru.twoida.basket_rush_client.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InviteScreen extends Activity {
+	
+	protected void onCreate (Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.invite_screen);
+	}
+}
